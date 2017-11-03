92feb12ef9d06c6f8ed519fd1870bb2a2a95661c
Maximiliano Lima <maxi.lima94@gmail.com>
2017-11-03 18:45:16 -0300

    CheckStyle InicioSesion.java

--- src/main/java/comandos/InicioSesion.java
+++ src/main/java/comandos/InicioSesion.java
@@ -7,23 +7,26 @@ import mensajeria.Paquete;
 import mensajeria.PaquetePersonaje;
 import mensajeria.PaqueteUsuario;
 import servidor.Servidor;
-
+/**
+ * 
+ * Inicio de sesion
+ *
+ */
 public class InicioSesion extends ComandosServer {
 
 	@Override
 	public void ejecutar() {
 		Paquete paqueteSv = new Paquete(null, 0);
 		paqueteSv.setComando(Comando.INICIOSESION);
-		
 		// Recibo el paquete usuario
 		escuchaCliente.setPaqueteUsuario((PaqueteUsuario) (gson.fromJson(cadenaLeida, PaqueteUsuario.class)));
-		
 		// Si se puede loguear el usuario le envio un mensaje de exito y el paquete personaje con los datos
 		try {
 			if (Servidor.getConector().loguearUsuario(escuchaCliente.getPaqueteUsuario())) {
 
 				PaquetePersonaje paquetePersonaje = new PaquetePersonaje();
-				paquetePersonaje = Servidor.getConector().getPersonaje(escuchaCliente.getPaqueteUsuario());
+				paquetePersonaje=Servidor.getConector().getPersonaje(
+						escuchaCliente.getPaqueteUsuario());
 				paquetePersonaje.setComando(Comando.INICIOSESION);
 				paquetePersonaje.setMensaje(Paquete.msjExito);
 				escuchaCliente.setIdPersonaje(paquetePersonaje.getId());
