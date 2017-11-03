d156dadd4d16d6a8efa81f9a42910e6b062dc660
Maximiliano Lima <maxi.lima94@gmail.com>
2017-11-03 18:55:21 -0300

    CheckStyle Atacar.java

--- src/main/java/comandos/Atacar.java
+++ src/main/java/comandos/Atacar.java
@@ -5,18 +5,23 @@ import java.io.IOException;
 import mensajeria.PaqueteAtacar;
 import servidor.EscuchaCliente;
 import servidor.Servidor;
-
+/**
+ * Clase Atacar
+ *
+ */
 public class Atacar extends ComandosServer {
 
 	@Override
 	public void ejecutar() {
 		escuchaCliente.setPaqueteAtacar((PaqueteAtacar) gson.fromJson(cadenaLeida, PaqueteAtacar.class));
-		for(EscuchaCliente conectado : Servidor.getClientesConectados()) {
-			if(conectado.getIdPersonaje() == escuchaCliente.getPaqueteAtacar().getIdEnemigo()) {
+		for (EscuchaCliente conectado : Servidor.getClientesConectados()) {
+			if (conectado.getIdPersonaje() == escuchaCliente.getPaqueteAtacar().getIdEnemigo()) {
 				try {
-					conectado.getSalida().writeObject(gson.toJson(escuchaCliente.getPaqueteAtacar()));
+					conectado.getSalida().writeObject(
+							gson.toJson(escuchaCliente.getPaqueteAtacar()));
 				} catch (IOException e) {
-					Servidor.log.append("Falló al intentar enviar ataque a:" + conectado.getPaquetePersonaje().getId() + "\n");
+					Servidor.log.append("Falló al intentar enviar ataque a:"
+							+conectado.getPaquetePersonaje().getId() + "\n");
 				}
 			}
 		}
