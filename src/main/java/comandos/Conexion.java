321a39e5b58c5654bdcb26978a803770d5b34c42
Maximiliano Lima <maxi.lima94@gmail.com>
2017-11-03 20:03:10 -0300

    CheckStyles Conexion.java

--- src/main/java/comandos/Conexion.java
+++ src/main/java/comandos/Conexion.java
@@ -3,20 +3,24 @@ package comandos;
 import mensajeria.PaqueteMovimiento;
 import mensajeria.PaquetePersonaje;
 import servidor.Servidor;
-
+/**
+ * 
+ * Clase conexion
+ *
+ */
 public class Conexion extends ComandosServer {
 
 	@Override
 	public void ejecutar() {
-		escuchaCliente.setPaquetePersonaje((PaquetePersonaje) (gson.fromJson(cadenaLeida, PaquetePersonaje.class)).clone());
-
-		Servidor.getPersonajesConectados().put(escuchaCliente.getPaquetePersonaje().getId(), (PaquetePersonaje) escuchaCliente.getPaquetePersonaje().clone());
-		Servidor.getUbicacionPersonajes().put(escuchaCliente.getPaquetePersonaje().getId(), (PaqueteMovimiento) new PaqueteMovimiento(escuchaCliente.getPaquetePersonaje().getId()).clone());
-		
-		synchronized(Servidor.atencionConexiones){
+		escuchaCliente.setPaquetePersonaje((PaquetePersonaje) 
+				(gson.fromJson(cadenaLeida, PaquetePersonaje.class)).clone());
+		Servidor.getPersonajesConectados().put(escuchaCliente.getPaquetePersonaje().getId(),
+				(PaquetePersonaje) escuchaCliente.getPaquetePersonaje().clone());
+		Servidor.getUbicacionPersonajes().put(escuchaCliente.getPaquetePersonaje().getId()
+				,(PaqueteMovimiento) new PaqueteMovimiento(
+				escuchaCliente.getPaquetePersonaje().getId()).clone());
+		synchronized (Servidor.atencionConexiones) {
 			Servidor.atencionConexiones.notify();
 		}
-
 	}
-
 }
