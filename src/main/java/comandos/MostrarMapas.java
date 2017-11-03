edf323b41113d260a4b4d2a8da38d278bca5ae8b
Maximiliano Lima <maxi.lima94@gmail.com>
2017-11-03 18:48:21 -0300

    CheckStyle MostrarMapas.java

--- src/main/java/comandos/MostrarMapas.java
+++ src/main/java/comandos/MostrarMapas.java
@@ -2,14 +2,22 @@ package comandos;
 
 import mensajeria.PaquetePersonaje;
 import servidor.Servidor;
-
-public class MostrarMapas extends ComandosServer{
+/**
+ * 
+ * Clase MostrarMapas
+ *
+ */
+public class MostrarMapas extends ComandosServer {
 
 	@Override
 	public void ejecutar() {
-		escuchaCliente.setPaquetePersonaje((PaquetePersonaje) gson.fromJson(cadenaLeida, PaquetePersonaje.class));
-		Servidor.log.append(escuchaCliente.getSocket().getInetAddress().getHostAddress() + " ha elegido el mapa " + escuchaCliente.getPaquetePersonaje().getMapa() + System.lineSeparator());
-		
+		escuchaCliente.setPaquetePersonaje((PaquetePersonaje)
+				gson.fromJson(cadenaLeida, PaquetePersonaje.class));
+		Servidor.log.append(escuchaCliente.getSocket()
+				.getInetAddress().getHostAddress()
+				+" ha elegido el mapa "
+				+escuchaCliente.getPaquetePersonaje().getMapa()
+				+System.lineSeparator());	
 	}
 
 }
