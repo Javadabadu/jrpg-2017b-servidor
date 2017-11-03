c92abdde14769c926e804aea31488f4ca82371f8
Maximiliano Lima <maxi.lima94@gmail.com>
2017-11-03 20:21:11 -0300

    CheckStyle Movimiento.java

--- src/main/java/comandos/Movimiento.java
+++ src/main/java/comandos/Movimiento.java
@@ -2,22 +2,27 @@ package comandos;
 
 import mensajeria.PaqueteMovimiento;
 import servidor.Servidor;
-
+/**
+ * 
+ * @author Javadabadu
+ *
+ */
 public class Movimiento extends ComandosServer {
 
 	@Override
 	public void ejecutar() {
-		escuchaCliente.setPaqueteMovimiento((PaqueteMovimiento) (gson.fromJson((String) cadenaLeida, PaqueteMovimiento.class)));
-		
-		Servidor.getUbicacionPersonajes().get(escuchaCliente.getPaqueteMovimiento().getIdPersonaje()).setPosX(escuchaCliente.getPaqueteMovimiento().getPosX());
-		Servidor.getUbicacionPersonajes().get(escuchaCliente.getPaqueteMovimiento().getIdPersonaje()).setPosY(escuchaCliente.getPaqueteMovimiento().getPosY());
-		Servidor.getUbicacionPersonajes().get(escuchaCliente.getPaqueteMovimiento().getIdPersonaje()).setDireccion(escuchaCliente.getPaqueteMovimiento().getDireccion());
-		Servidor.getUbicacionPersonajes().get(escuchaCliente.getPaqueteMovimiento().getIdPersonaje()).setFrame(escuchaCliente.getPaqueteMovimiento().getFrame());
-		
-		synchronized(Servidor.atencionMovimientos){
+		escuchaCliente.setPaqueteMovimiento((PaqueteMovimiento)
+				(gson.fromJson((String) cadenaLeida, PaqueteMovimiento.class)));
+		Servidor.getUbicacionPersonajes().get(escuchaCliente.getPaqueteMovimiento().getIdPersonaje()
+											.setPosX(escuchaCliente.getPaqueteMovimiento().getPosX());
+		Servidor.getUbicacionPersonajes().get(escuchaCliente.getPaqueteMovimiento().getIdPersonaje())
+											.setPosY(escuchaCliente.getPaqueteMovimiento().getPosY());
+		Servidor.getUbicacionPersonajes().get(escuchaCliente.getPaqueteMovimiento().getIdPersonaje())
+											.setDireccion(escuchaCliente.getPaqueteMovimiento().getDireccion());
+		Servidor.getUbicacionPersonajes().get(escuchaCliente.getPaqueteMovimiento().getIdPersonaje())
+											.setFrame(escuchaCliente.getPaqueteMovimiento().getFrame());
+		synchronized (Servidor.atencionMovimientos) {
 			Servidor.atencionMovimientos.notify();
 		}
-
 	}
-
 }
