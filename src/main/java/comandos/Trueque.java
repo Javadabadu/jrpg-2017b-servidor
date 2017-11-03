242d0369143d82c2de183bff795c7a49b9fd5cb9
Maximiliano Lima <maxi.lima94@gmail.com>
2017-11-03 20:19:23 -0300

    CheckStyle Trueque.java

--- src/main/java/comandos/Trueque.java
+++ src/main/java/comandos/Trueque.java
@@ -5,7 +5,11 @@ import java.io.IOException;
 import mensajeria.PaqueteComerciar;
 import servidor.EscuchaCliente;
 import servidor.Servidor;
-
+/**
+ * 
+ * @author Javadabadu
+ *
+ */
 public class Trueque extends ComandosServer {
 
 	@Override
@@ -13,22 +17,23 @@ public class Trueque extends ComandosServer {
 		PaqueteComerciar paqueteComerciar;
 		paqueteComerciar = (PaqueteComerciar) gson.fromJson(cadenaLeida, PaqueteComerciar.class);
 		//BUSCO EN LAS ESCUCHAS AL QUE SE LO TENGO QUE MANDAR
-		for(EscuchaCliente conectado : Servidor.getClientesConectados()) {
-			if(conectado.getPaquetePersonaje().getId() == paqueteComerciar.getIdEnemigo()) {
+		for (EscuchaCliente conectado : Servidor.getClientesConectados()) {
+			if (conectado.getPaquetePersonaje().getId() == paqueteComerciar.getIdEnemigo()) {
 				try {
 					conectado.getSalida().writeObject(gson.toJson(paqueteComerciar));
 				} catch (IOException e) {
-					Servidor.log.append("Falló al intentar enviar trueque a:" + conectado.getPaquetePersonaje().getId() + "\n");
-				}	
-			} 
-			else if(conectado.getPaquetePersonaje().getId() == paqueteComerciar.getId()) {
+					Servidor.log.append("Falló al intentar enviar trueque a:"
+							+ conectado.getPaquetePersonaje().getId() + "\n");
+				}
+		}
+			else if (conectado.getPaquetePersonaje().getId() == paqueteComerciar.getId()) {
 				try {
 					conectado.getSalida().writeObject(gson.toJson(paqueteComerciar));
 				} catch (IOException e) {
-					Servidor.log.append("Falló al intentar enviar trueque a:" + conectado.getPaquetePersonaje().getId() + "\n");
+					Servidor.log.append("Falló al intentar enviar trueque a:"
+				+ conectado.getPaquetePersonaje().getId() + "\n");
 				}
 			}
 		}
 	}
-
 }
