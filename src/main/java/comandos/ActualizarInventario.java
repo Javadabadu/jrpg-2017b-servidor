61e3f660a9c2f67a7a1b26ed2acebafa0b53150d
Maximiliano Lima <maxi.lima94@gmail.com>
2017-11-03 20:17:16 -0300

    CheckStyle ActualizarInventario.java

--- src/main/java/comandos/ActualizarInventario.java
+++ src/main/java/comandos/ActualizarInventario.java
@@ -5,25 +5,28 @@ import java.io.IOException;
 import mensajeria.PaquetePersonaje;
 import servidor.EscuchaCliente;
 import servidor.Servidor;
-
+/**
+ * 
+ * @author Javadabadu
+ *
+ */
 public class ActualizarInventario extends ComandosServer {
 
 	@Override
 	public void ejecutar() {
-		escuchaCliente.setPaquetePersonaje((PaquetePersonaje) gson.fromJson(cadenaLeida, PaquetePersonaje.class));
-		
+		escuchaCliente.setPaquetePersonaje((PaquetePersonaje)
+				gson.fromJson(cadenaLeida, PaquetePersonaje.class));
 		Servidor.getConector().actualizarInventario(escuchaCliente.getPaquetePersonaje());
 		Servidor.getPersonajesConectados().remove(escuchaCliente.getPaquetePersonaje().getId());
-		Servidor.getPersonajesConectados().put(escuchaCliente.getPaquetePersonaje().getId(), escuchaCliente.getPaquetePersonaje());
-		
-		for(EscuchaCliente conectado : Servidor.getClientesConectados()) {
+		Servidor.getPersonajesConectados().put(escuchaCliente.getPaquetePersonaje().getId()
+				, escuchaCliente.getPaquetePersonaje());
+		for (EscuchaCliente conectado : Servidor.getClientesConectados()) {
 			try {
 				conectado.getSalida().writeObject(gson.toJson(escuchaCliente.getPaquetePersonaje()));
 			} catch (IOException e) {
-				Servidor.log.append("Falló al intentar enviar paquetePersonaje a:" + conectado.getPaquetePersonaje().getId() + "\n");
+				Servidor.log.append("Falló al intentar enviar paquetePersonaje a:"
+						+ conectado.getPaquetePersonaje().getId() + "\n");
 			}
 		}
-
 	}
-
 }
--- src/main/java/comandos/ActualizarPersonaje.java
+++ src/main/java/comandos/ActualizarPersonaje.java
@@ -28,7 +28,5 @@ public class ActualizarPersonaje extends ComandosServer {
 						+ conectado.getPaquetePersonaje().getId() + "\n");
 			}
 		}
-
 	}
-
 }
