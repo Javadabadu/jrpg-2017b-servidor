dab81554bbe927936eb03fa33f713ace44bf5f55
Juan Manuel Martin Mora <jmartinmora5@gmail.com>
2017-11-04 16:43:13 -0300

    Ahora tira error por una property duplicada, la cual no existe :S

--- src/main/java/servidor/HibernateConector.java
+++ src/main/java/servidor/HibernateConector.java
@@ -5,37 +5,36 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 /**
-*
-* @author Grupo javadabaduu
-*/
+ *
+ * @author Grupo javadabaduu
+ */
 public class HibernateConector {
-  
-  private static Session session ;
-  private SessionFactory factory ;
-  /*
-   * Uso del patron *Singleton* para la obtencion de una sola session. 
-   * */
-  /*
-   * Constructor privado
-   */
- private HibernateConector(){
-   
-   Configuration cfg = new Configuration();
-   cfg.configure("hibernate.cfg.xml");
 
-    factory = cfg.buildSessionFactory();
-    session = factory.openSession();
- 
-   
- }
- 
- public static Session obtenerSession(){
-  
-   if(session == null)
-     new HibernateConector();
-   
- return session;
-}
- 
- 
+	private static Session session;
+	private SessionFactory factory;
+
+	/*
+	 * Uso del patron *Singleton* para la obtencion de una sola session.
+	 */
+	/*
+	 * Constructor privado
+	 */
+	private HibernateConector() {
+
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+
+		factory = cfg.buildSessionFactory();
+		session = factory.openSession();
+
+	}
+
+	public static Session obtenerSession() {
+
+		if (session == null)
+			new HibernateConector();
+
+		return session;
+	}
+
 }
