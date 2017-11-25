d542742118972bb7ab47067dded1314717e5efa1
Maximiliano Lima <maxi.lima94@gmail.com>
2017-11-25 05:22:31 -0300

    Se modifican el classpath y algunas pos de NPC

--- src/main/java/servidor/AtencionNPC.java
+++ src/main/java/servidor/AtencionNPC.java
@@ -13,17 +13,11 @@ public class AtencionNPC {
 	public static void cargarNPC(){
 		
 		
-		
-//		PaqueteNpc personajeNPC = new PaqueteNpc(0, "Lucaneitor", PaqueteNpc.LUCANEITOR, 2,1, 20, 70, Estado.estadoJuego,1,1);	
-//		Servidor.getPersonajesNPC().put(0, personajeNPC);
-//		PaqueteNpc personajeNPC2 = new PaqueteNpc(1, "Leo-nidas", PaqueteNpc.LEONIDAS, 2, 1, 50, 100, Estado.estadoJuego,1,1);
-//		Servidor.getPersonajesNPC().put(1, personajeNPC2);
-		
 		PaqueteNpc lucas = new PaqueteNpc(0, "Lucaneitor", PaqueteNpc.LUCANEITOR, 2,1, -1, 1000, Estado.getEstadoJuego(),1,1 );
 		Servidor.getPersonajesNPC().put(0, lucas);
 		PaqueteNpc lucas1 = new PaqueteNpc(1, "Lucaneitor1", PaqueteNpc.LUCANEITOR, 2,1, 2000, 1200, Estado.getEstadoJuego(),1,1 );
 		Servidor.getPersonajesNPC().put(1, lucas1);
-		PaqueteNpc lucas2 = new PaqueteNpc(2, "Lucaneitor2", PaqueteNpc.LUCANEITOR, 2,1, 400, 700, Estado.getEstadoJuego(),1,1 );
+		PaqueteNpc lucas2 = new PaqueteNpc(2, "Lucaneitor2", PaqueteNpc.LUCANEITOR, 2,1, 500, 700, Estado.getEstadoJuego(),1,1 );
 		Servidor.getPersonajesNPC().put(2, lucas2);
 		PaqueteNpc lucas3 = new PaqueteNpc(3, "Lucaneitor3", PaqueteNpc.LUCANEITOR, 2,1, 60, 1500, Estado.getEstadoJuego(),1,1 );
 		Servidor.getPersonajesNPC().put(3, lucas3);
@@ -31,15 +25,15 @@ public class AtencionNPC {
 		Servidor.getPersonajesNPC().put(4, lucas4);
 		
 		
-		PaqueteNpc leo = new PaqueteNpc(5, "Leo-nidas", PaqueteNpc.LEONIDAS, 2, 1, -2000, 1130, Estado.getEstadoJuego(),1,1);
+		PaqueteNpc leo = new PaqueteNpc(5, "Leo-nidas", PaqueteNpc.LEONIDAS, 2, 1, -1850, 1130, Estado.getEstadoJuego(),1,1);
 		Servidor.getPersonajesNPC().put(5, leo);
-		PaqueteNpc leo1 = new PaqueteNpc(6, "Leo-nidas1", PaqueteNpc.LEONIDAS, 2, 1, -1000, 800, Estado.getEstadoJuego(),1,1);
+		PaqueteNpc leo1 = new PaqueteNpc(6, "Leo-nidas1", PaqueteNpc.LEONIDAS, 2, 1, -850, 800, Estado.getEstadoJuego(),1,1);
 		Servidor.getPersonajesNPC().put(6, leo1);
 		PaqueteNpc leo2 = new PaqueteNpc(7, "Leo-nidas2", PaqueteNpc.LEONIDAS, 2, 1, -20, 1800, Estado.getEstadoJuego(),1,1);
 		Servidor.getPersonajesNPC().put(7, leo2);
 		PaqueteNpc leo3 = new PaqueteNpc(8, "Leo-nidas3", PaqueteNpc.LEONIDAS, 2, 1, 1500, 630, Estado.getEstadoJuego(),1,1);
 		Servidor.getPersonajesNPC().put(8, leo3);
-		PaqueteNpc leo4 = new PaqueteNpc(9, "Leo-nidas4", PaqueteNpc.LEONIDAS, 2, 1, 2000, 600, Estado.getEstadoJuego(),1,1);
+		PaqueteNpc leo4 = new PaqueteNpc(9, "Leo-nidasPruebas", PaqueteNpc.LEONIDAS, 2, 1, 50, 250, Estado.getEstadoJuego(),1,1);
 		Servidor.getPersonajesNPC().put(9, leo4);
 		
 		
